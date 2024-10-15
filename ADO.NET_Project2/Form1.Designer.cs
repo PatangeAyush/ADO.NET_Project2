ce98a0b4818fc146ffc9c4f813bd1c36ab169bcb
PatangeAyush <patangeayush977@gmail.com>
2024-10-15 14:40:18 +0530

    Add project files.

---
+++ ADO.NET_Project2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ADO.NET_Project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCusId = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPayMethod = new System.Windows.Forms.Label();
+            this.lblCusName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtCusId = new System.Windows.Forms.TextBox();
+            this.txtCusName = new System.Windows.Forms.TextBox();
+            this.txtPayMethod = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnSelectCustomer = new System.Windows.Forms.Button();
+            this.btnDeleteCustomer = new System.Windows.Forms.Button();
+            this.btnAddCustomer = new System.Windows.Forms.Button();
+            this.btnUpdateCustomer = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCusId
+            // 
+            this.lblCusId.AutoSize = true;
+            this.lblCusId.Location = new System.Drawing.Point(12, 24);
+            this.lblCusId.Name = "lblCusId";
+            this.lblCusId.Size = new System.Drawing.Size(43, 13);
+            this.lblCusId.TabIndex = 0;
+            this.lblCusId.Text = "CUS ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // lblPayMethod
+            // 
+            this.lblPayMethod.AutoSize = true;
+            this.lblPayMethod.Location = new System.Drawing.Point(416, 24);
+            this.lblPayMethod.Name = "lblPayMethod";
+            this.lblPayMethod.Size = new System.Drawing.Size(64, 13);
+            this.lblPayMethod.TabIndex = 2;
+            this.lblPayMethod.Text = "Pay Method";
+            // 
+            // lblCusName
+            // 
+            this.lblCusName.AutoSize = true;
+            this.lblCusName.Location = new System.Drawing.Point(205, 24);
+            this.lblCusName.Name = "lblCusName";
+            this.lblCusName.Size = new System.Drawing.Size(56, 13);
+            this.lblCusName.TabIndex = 3;
+            this.lblCusName.Text = "Cus Name";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(621, 24);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtCusId
+            // 
+            this.txtCusId.Location = new System.Drawing.Point(61, 17);
+            this.txtCusId.Name = "txtCusId";
+            this.txtCusId.Size = new System.Drawing.Size(100, 20);
+            this.txtCusId.TabIndex = 5;
+            // 
+            // txtCusName
+            // 
+            this.txtCusName.Location = new System.Drawing.Point(267, 17);
+            this.txtCusName.Name = "txtCusName";
+            this.txtCusName.Size = new System.Drawing.Size(100, 20);
+            this.txtCusName.TabIndex = 6;
+            // 
+            // txtPayMethod
+            // 
+            this.txtPayMethod.Location = new System.Drawing.Point(486, 17);
+            this.txtPayMethod.Name = "txtPayMethod";
+            this.txtPayMethod.Size = new System.Drawing.Size(100, 20);
+            this.txtPayMethod.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(672, 17);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // btnSelectCustomer
+            // 
+            this.btnSelectCustomer.Location = new System.Drawing.Point(624, 61);
+            this.btnSelectCustomer.Name = "btnSelectCustomer";
+            this.btnSelectCustomer.Size = new System.Drawing.Size(107, 29);
+            this.btnSelectCustomer.TabIndex = 9;
+            this.btnSelectCustomer.Text = "Select Customer";
+            this.btnSelectCustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteCustomer
+            // 
+            this.btnDeleteCustomer.Location = new System.Drawing.Point(208, 61);
+            this.btnDeleteCustomer.Name = "btnDeleteCustomer";
+            this.btnDeleteCustomer.Size = new System.Drawing.Size(97, 29);
+            this.btnDeleteCustomer.TabIndex = 10;
+            this.btnDeleteCustomer.Text = "Delete Customer";
+            this.btnDeleteCustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnAddCustomer
+            // 
+            this.btnAddCustomer.Location = new System.Drawing.Point(15, 61);
+            this.btnAddCustomer.Name = "btnAddCustomer";
+            this.btnAddCustomer.Size = new System.Drawing.Size(88, 29);
+            this.btnAddCustomer.TabIndex = 11;
+            this.btnAddCustomer.Text = "AddCustomer";
+            this.btnAddCustomer.UseVisualStyleBackColor = true;
+            this.btnAddCustomer.Click += new System.EventHandler(this.btnAddCustomer_Click);
+            // 
+            // btnUpdateCustomer
+            // 
+            this.btnUpdateCustomer.Location = new System.Drawing.Point(419, 61);
+            this.btnUpdateCustomer.Name = "btnUpdateCustomer";
+            this.btnUpdateCustomer.Size = new System.Drawing.Size(104, 29);
+            this.btnUpdateCustomer.TabIndex = 12;
+            this.btnUpdateCustomer.Text = "Update Customer";
+            this.btnUpdateCustomer.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(796, 296);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnUpdateCustomer);
+            this.Controls.Add(this.btnAddCustomer);
+            this.Controls.Add(this.btnDeleteCustomer);
+            this.Controls.Add(this.btnSelectCustomer);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtPayMethod);
+            this.Controls.Add(this.txtCusName);
+            this.Controls.Add(this.txtCusId);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblCusName);
+            this.Controls.Add(this.lblPayMethod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCusId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCusId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPayMethod;
+        private System.Windows.Forms.Label lblCusName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtCusId;
+        private System.Windows.Forms.TextBox txtCusName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnSelectCustomer;
+        private System.Windows.Forms.Button btnDeleteCustomer;
+        private System.Windows.Forms.Button btnAddCustomer;
+        private System.Windows.Forms.Button btnUpdateCustomer;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtPayMethod;
+    }
+}
+
---
+++ ADO.NET_Project2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO.NET_Project2
+{
+    public partial class Form1 : Form
+    {
+        // Connection String
+        string ConStr = "Data Source = DESKTOP-T4LB3IA\\SQLEXPRESS; Initial Catalog = STUDENT; Integrated Security = True;";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConStr);
+
+            try
+            {
+                string AddCustomersql = @"Insert Into CUSTOMER (CUS_ID, CUS_NAME, PAY_METHOD,ADDRESS)
+                                   VALUES( @CUSID, @CUSNAME, @PAYMETHOD,@ADDRESS )";
+
+                SqlCommand cmd = new SqlCommand(AddCustomersql, con);
+
+                cmd.Parameters.AddWithValue("@CUSID", txtCusId.Text);
+                cmd.Parameters.AddWithValue("@CUSNAME", txtCusName.Text);
+                cmd.Parameters.AddWithValue("@PAYMETHOD", txtPayMethod.Text);
+                cmd.Parameters.AddWithValue("@ADDRESS", txtAddress.Text);
+
+                con.Open();
+
+                int rowEffected = cmd.ExecuteNonQuery();
+
+                if (rowEffected > 0)
+                {
+                    MessageBox.Show("Record Inserted Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Record Was Not Inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An Error Occured : "+ ex);
+            }
+            finally 
+            {
+                con.Close();
+            } 
+        }
+    }
+}
